c4640616b480373dcd71f92a592f4095ee44b230
Vivasvan Patel <vivasvanpatel40@gmail.com>
2018-10-11 10:15:35 +0530

    filename corrections

---
+++ ques3_1.cpp
@@ -0,0 +1,26 @@
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    union {
+        int i;
+        char c[sizeof(int)];
+    } x;
+    
+    x.i = 1;
+    if(x.c[0] == 1){cout<< "little-endian\n"<<endl;}
+    else{cout<<"big-endian\n"<<endl;}
+    return 0;
+
+    ntohl()
+
+
+}
